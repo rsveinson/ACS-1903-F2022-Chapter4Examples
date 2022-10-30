1b0dcc3e06d9c63d241fa0a4210d8546c1e0e003
rsveinson <rsveinson@mts.net>
2022-10-30 17:03:07 -0500

    tidied up the CalcuateFactorial example

--- CalculateFactorial.java
+++ CalculateFactorial.java
@@ -4,26 +4,34 @@ import java.util.Scanner;
  *      Use a while to calculate n! 
  *      Prompt the user for the value of n
  */
-public class Ch4Q10
+public class CalculateFactorial
 {
     public static void main(String[] args){
-        int n = askUserForN();
-        int originalN = n;
-        int nFactorial = 1;
+        // *** variables
+        int n = 0;          // this is the number used to calculate factotia
+        int originalN = n;  // keep track of the original number
+        int nFactorial = 1; // this is the factorials of n
+        
+        // *** get some intput
+        n = askUserForN();      // call the method to get the input
+        
+        // Calculate the factorial
         while (n > 0){
             nFactorial = nFactorial * n;
             n = n-1;
-        }
+        }// end while
+        
+        // print the result
         System.out.println("The value of "
             +n
             +"! is: "
             +nFactorial);
-    }
+    }// end main
 
     public static int askUserForN(){
         Scanner kb = new Scanner(System.in);
         System.out.println("Please enter a value for n");
         int n = kb.nextInt();
         return n;
-    }
-}
+    }// end get input
+}// end class
